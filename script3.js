3ab38a1966f30564e301adc6fe4a5182b96365eb
csawin0122 <rooneysoccer22@gmail.com>
2024-11-18 20:30:59 -0500

    Added Points (with color scheme) to Map
    Added a point for each entry in the dataset and a color scheme to color the points based on price

--- script3.js
+++ script3.js
@@ -1,9 +1,6 @@
 d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
     d3.json("Boroughs.geojson").then(function(mapdata){
 
-        console.log(dataset)
-        console.log(mapdata)
-
         var dimensions = {
             height: 850,
             width: 800,
@@ -15,6 +12,9 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
             }
         }
 
+        dataset.forEach(d => {d.price = +d.price.replace(/[$,]/g, "")});
+        dataset = dataset.filter(d => d.long < 0 && d.lat > 0 && +d.price > 0)
+
         var svg = d3.select("#Map")
                 .style("width", dimensions.width)
                 .style("height", dimensions.height)
@@ -25,7 +25,11 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
 
         var pathGenerator = d3.geoPath(projection)
 
-        var colorScale = d3.scaleOrdinal(d3.schemeSet1)
+        var colorScale1 = d3.scaleOrdinal(d3.schemeSet2)
+        
+        // white = lower price -- Red = higher price
+        var colorScale2 = d3.scaleSequential(d3.interpolateReds)
+                            .domain(d3.extent(dataset, d => +d.price))
 
         var boroughs = svg.append("g")
                           .selectAll(".boroughs")
@@ -34,8 +38,30 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
                           .append("path")
                           .attr("class", "boroughs")
                           .attr("d", d => pathGenerator(d))
-                          .attr("stroke", "white")
-                          .attr("fill", d => colorScale(d.properties['boro_name']))
+                          .attr("stroke", "black")
+                          .attr("fill", d => colorScale1(d.properties['boro_name']))
+        
+        var points = svg.append("g")
+                        .selectAll(".points")
+                        .data(dataset)
+                        .enter()
+                        .filter(function(d) {
+                            return mapdata.features.some(function(borough) {
+                                return d3.geoContains(borough, [+d.long, +d.lat]);
+                            });
+                        })
+                        .append("circle")
+                        .attr("class", "points")
+                        .attr("cx", d => {
+                            coords = projection([+d.long, +d.lat])
+                            return coords ? coords[0] : null
+                        })
+                        .attr("cy", d => {
+                            coords = projection([+d.long, +d.lat])
+                            return coords ? coords[1] : null
+                        })
+                        .attr("r", 1.5)
+                        .attr("fill", d => colorScale2(+d.price))
 
         var title = svg.append("text")
                        .attr("x", dimensions.width / 2)
