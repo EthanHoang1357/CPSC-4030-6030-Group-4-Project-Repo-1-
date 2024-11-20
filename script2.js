aac70b42912a3ff661636342442de8a409b7b2f0
csawin0122 <rooneysoccer22@gmail.com>
2024-11-19 19:04:43 -0500

    Fixed bar chart based on Dr F's feedback
    Grouped the neighborhoods by Borough and colored by borough. Also added some interaction where when the user hovers over a bar, the name of the neighborhood is displayed as well as the avg review rating for that neighborhood

--- script2.js
+++ script2.js
@@ -12,18 +12,29 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
     }
 
     // Only showing neighbourhoods in the Bronx for now as a reference
-    dataset = dataset.filter(d => d['neighbourhood group'] === 'Bronx' && d.neighbourhood && !isNaN(+d['review rate number']) && +d['review rate number'] > 0)
+    //dataset = dataset.filter(d => d['neighbourhood group'] === 'Bronx' && d.neighbourhood && !isNaN(+d['review rate number']) && +d['review rate number'] > 0)
 
     // Showing all neighborhoods
-    //dataset = dataset.filter(d => d.neighbourhood && !isNaN(+d['review rate number']) && +d['review rate number'] > 0)
+    dataset = dataset.filter(d => d["neighbourhood group"] && d.neighbourhood && !isNaN(+d['review rate number']) && +d['review rate number'] > 0)
+
+    dataset.forEach(d => {
+        if (d["neighbourhood group"]) {
+            if (d["neighbourhood group"] === "brookln") d["neighbourhood group"] = "Brooklyn"
+            if (d["neighbourhood group"] === "manhatan") d["neighbourhood group"] = "Manhattan"
+        }
+    })
 
     var avgReviewByNeighborhood = d3.rollup(dataset, 
-        v => d3.mean(v, d => +d["review rate number"]), 
+        v => d3.mean(v, d => +d["review rate number"]),
+        d => d["neighbourhood group"],
         d => d["neighbourhood"]
     )
 
-    var data = Array.from(avgReviewByNeighborhood, ([key, value]) => ({ neighbourhood: key, avgReview: value }))
-    data.sort((a, b) => d3.ascending(a.neighbourhood, b.neighbourhood))
+    var data = Array.from(avgReviewByNeighborhood, ([borough, neighbourhoods]) => 
+        Array.from(neighbourhoods, ([neighbourhood, avgReview]) => ({ borough, neighbourhood, avgReview }))
+    ).flat()
+
+    data.sort((a, b) => d3.ascending(a.borough, b.borough) || d3.ascending(a.neighbourhood, b.neighbourhood))
 
     var svg = d3.select("#BarChart")
                 .style("width", dimensions.width)
@@ -38,6 +49,8 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
                    .domain([1.5, d3.max(data, d => d.avgReview)])
                    .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
 
+    var colorScale = d3.scaleOrdinal(d3.schemeSet1)
+
     var bars = svg.append("g")
                   .selectAll("rect")
                   .data(data)
@@ -47,39 +60,46 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
                   .attr("y", d => yScale(d.avgReview))
                   .attr("height", d => dimensions.height - yScale(d.avgReview) - dimensions.margin.bottom)
                   .attr("width", d => xScale.bandwidth())
-                  .attr("fill", "black")
-
-    var textLabels = svg.append("g")
-                        .selectAll("text")
-                        .data(data)
-                        .enter()
-                        .append("text")
-                        .attr("x", d => xScale(d.neighbourhood) + xScale.bandwidth() / 2)  // Center the text over each bar
-                        .attr("y", d => yScale(d.avgReview) - 5)  // Position the text just above the bar
-                        .attr("text-anchor", "middle")
-                        .style("font-size", "12px")
-                        .text(d => d.avgReview.toFixed(2))
-                        .attr("transform", d => "rotate(270," + (xScale(d.neighbourhood) + xScale.bandwidth() / 2 - 3) + "," + (yScale(d.avgReview) - 12) + ")")
-
+                  .attr("fill", d => colorScale(d.borough))
+                  .on("mouseover", function(d, i){
+                        d3.select(this).style("stroke", "black")
+                        Neighborhoodtext.text(`Neighborhood: ${i.neighbourhood}`)
+                        AvgRRtext.text(`Average Review Rating: ${i.avgReview.toFixed(3)}`)
+                  })
+                  .on("mouseout", function(d, i){
+                        d3.select(this).style("stroke", "none")
+                        Neighborhoodtext.text('Neighborhood:')
+                        AvgRRtext.text(`Average Review Rating:`)
+                  })
 
     var xAxisGen = d3.axisBottom().scale(xScale)
     var xAxis = svg.append("g")
                    .call(xAxisGen)
                    .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`)
 
-    xAxis.selectAll("text")
-         .style("text-anchor", "end")
-         .attr("dx", "-0.8em")
-         .attr("dy", ".15em")
-         .attr("transform", "rotate(-65)")
-         .style("font-size", "12px")
+    xAxis.selectAll("text").remove()
+
+    var boroughs = d3.group(data, d => d.borough)
+
+    boroughs.forEach((neighborhoods, borough) => {
+        var firstNeighborhood = neighborhoods[0]
+        var lastNeighborhood = neighborhoods[neighborhoods.length - 1]
+        var xPosition = (xScale(firstNeighborhood.neighbourhood) + xScale(lastNeighborhood.neighbourhood)) / 2 + xScale.bandwidth() / 2
+
+        svg.append("text")
+           .attr("x", xPosition)
+           .attr("y", dimensions.height - dimensions.margin.bottom + 30)
+           .attr("text-anchor", "middle")
+           .style("font-size", "14px")
+           .text(borough)
+    })
 
     var xAxisText = svg.append("text")
                        .attr("x", dimensions.width / 2)
-                       .attr("y", dimensions.height - 10)
+                       .attr("y", dimensions.height - 75)
                        .attr("fill", "black")
                        .style("font-size", "16px")
-                       .text("Neighbourhood")
+                       .text("Borough/Neighborhood")
 
     var yAxisGen = d3.axisLeft().scale(yScale)
     var yAxis = svg.append("g")
@@ -102,6 +122,24 @@ d3.csv("Airbnb_Open_Data.csv").then(function(dataset) {
                    .attr("y", dimensions.margin.top / 2)
                    .attr("text-anchor", "middle")
                    .style("font-size", "24px")
-                   .text("Avg Review Rating per Neighborhood (Bronx)")
-                   //.text("Avg Review Rating per Neighborhood")
+                   //.text("Avg Review Rating per Neighborhood (Bronx)")
+                   .text("Avg Review Rating per Neighborhood")
+
+    var Neighborhoodtext = svg.append('text')
+                              .attr("id", 'Neighborhoodtext')
+                              .attr("x", dimensions.width / 2)
+                              .attr("y", dimensions.margin.top / 2 + 30)
+                              .attr("dx", "-.8em")
+                              .attr("dy", ".15em")
+                              .attr("font-family", "sans-serif")
+                              .text("Neighborhood:")
+
+    var AvgRRtext = svg.append('text')
+                              .attr("id", 'AvgRRtext')
+                              .attr("x", dimensions.width / 2 - 30)
+                              .attr("y", dimensions.margin.top / 2 + 50)
+                              .attr("dx", "-.8em")
+                              .attr("dy", ".15em")
+                              .attr("font-family", "sans-serif")
+                              .text("Average Review Rating:")
 })
