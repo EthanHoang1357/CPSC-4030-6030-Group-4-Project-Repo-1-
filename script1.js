70619dfc89f4bad107c7cf30491c69e3e373755c
chesbulla <chesterchua722@gmail.com>
2024-11-25 20:17:31 -0500

    Add scatter plot

--- script1.js
+++ script1.js
@@ -13,29 +13,59 @@ d3.csv("Airbnb_Open_Data.csv").then(
         }
 
         dataset.forEach(d => {
-            d.price = +d.price.replace(/[$,]/g, "")
-            d['Construction year'] = +d['Construction year']
+            d['service fee'] = +d['service fee'].replace(/[$,]/g, "")
+            d['minimum nights'] = +d['minimum nights']
         });
 
-        dataset = dataset.filter(d => !isNaN(d.price) && !isNaN(d['Construction year']) && d['Construction year'] > 0)
+        const groupedData = d3.flatRollup(
 
+            dataset,
+            v => ({
+                avgServiceFee: d3.mean(v, d => d['service fee']),
+
+
+
+            }),
+            d => d['minimum nights'],
+            d => d['room type']
+
+        );
+
+        dataset.forEach(d => {
+            if (d.roomType === "") { 
+                d.roomType = "Unknown"; 
+            }
+        });
+        const processedData = groupedData.map(([minNights, roomType, values]) => ({
+            minNights,
+            roomType,
+            avgServiceFee: values.avgServiceFee
+        }));
+
+       
+        const colorScale = d3.scaleOrdinal()
+                        .domain([...new Set(dataset.map(d => d['room type']))])
+                        .range(d3.schemeCategory10);
+
+        const roomTypes = colorScale.domain();
+        console.log([...new Set(dataset.map(d => d['room type']))]);
         var svg = d3.select("#ScatterPlot")
                     .style("width", dimensions.width)
                     .style("height", dimensions.height)
 
         var xScale = d3.scaleLinear()
-                       .domain(d3.extent(dataset, d => d['Construction year']))
+                       .domain(d3.extent(processedData, d => d.avgServiceFee))
                        .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
 
         console.log("xScale domain:", xScale.domain());
         console.log("xScale range:", [dimensions.margin.left, dimensions.width - dimensions.margin.right]);
 
         var yScale = d3.scaleLinear()
-                       .domain(d3.extent(dataset, d => d.price))
+                       .domain(d3.extent(processedData, d => d.minNights))
                        .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
 
         var xAxisGen = d3.axisBottom().scale(xScale)
-                         .tickFormat(d3.format("d"))
+                         .tickFormat(d3.format("$d"))
 
         var xAxis = svg.append("g")
                        .attr("transform", `translate(0, ${dimensions.height - dimensions.margin.bottom})`)
@@ -46,10 +76,10 @@ d3.csv("Airbnb_Open_Data.csv").then(
                            .attr("y", dimensions.height - 10)
                            .attr("fill", "black")
                            .style("font-size", "16px")
-                           .text("Construction Year")
+                           .text("Average Service Fee")
 
         var yAxisGen = d3.axisLeft().scale(yScale)
-                         .tickFormat(d3.format("$d"))
+                         .tickFormat(d3.format("d"))
 
         var yAxis = svg.append("g")
                        .attr("transform", `translate(${dimensions.margin.left}, 0)`)
@@ -61,23 +91,53 @@ d3.csv("Airbnb_Open_Data.csv").then(
                            .attr("y", 15)
                            .attr("fill", "black")
                            .style("font-size", "16px")
-                           .text("Price")
+                           .text("Minimum Nights")
 
         var PlotPoints = svg.append("g")
                             .selectAll("dot")
-                            .data(dataset)
+                            .data(processedData)
                             .enter()
                             .append("circle")
-                            .attr("cx", d => xScale(d['Construction year']))
-                            .attr("cy", d => yScale(d.price))
+                            .attr("cx", d => xScale(d.avgServiceFee))
+                            .attr("cy", d => yScale(d.minNights))
                             .attr("r", 1.5)
-                            .attr("fill", "black")
+                            .attr("fill", d => colorScale(d.roomType)); // Apply color based on room type
 
+        const legend = svg.append("g")
+                        .attr("transform", `translate(${dimensions.width - dimensions.margin.left - dimensions.margin.right-80}, 40)`)
+                        .attr("class", "legend");
+
+      
+    const legendRectSize = 10; 
+    const legendSpacing = 5;   
+    
+    
+    legend.selectAll("rect")
+        .data(roomTypes) 
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", (d, i) => i * (legendRectSize + legendSpacing)) 
+        .attr("width", legendRectSize)
+        .attr("height", legendRectSize)
+        .attr("fill", d => colorScale(d));
+    
+
+    legend.selectAll("text")
+        .data(roomTypes)
+        .enter()
+        .append("text")
+        .attr("x", legendRectSize + 5) 
+        .attr("y", (d, i) => i * (legendRectSize + legendSpacing) + legendRectSize / 2)
+        .attr("dy", "0.35em")
+        .style("font-size", "10px") 
+        .text(d => d);
+                        
         var title = svg.append("text")
                        .attr("x", dimensions.width / 2)
                        .attr("y", dimensions.margin.top / 2)
                        .attr("text-anchor", "middle")
                        .style("font-size", "24px")
-                       .text("Price vs Construction Year Scatterplot")
+                       .text("Average Service Fee vs Minimum Nights Scatterplot")
     }
 )
